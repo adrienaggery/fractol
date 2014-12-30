f207d47c5409c54155c5a2673cbdbd59c8afd1c7
Adrien AGGERY <adrien@aggery.com>
2014-12-30 19:56:03 +0100

    ft_init ++

--- src/fractol.c
+++ src/fractol.c
@@ -6,14 +6,25 @@
 /*   By: aaggery <aaggery@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/12/30 13:35:20 by aaggery           #+#    #+#             */
-/*   Updated: 2014/12/30 14:16:53 by aaggery          ###   ########.fr       */
+/*   Updated: 2014/12/30 19:55:31 by aaggery          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-static void		ft_init(t_fractol fl)
+static void		ft_init(t_fractol *fl)
 {
+	fl->mlx = mlx_init();
+	fl->win = mlx_new_window(fl->mlx, WIN_WIDTH, WIN_HEIGHT, "Fract'ol");
+	if (ft_strequ("mandelbrot", argv[1]))
+		mlx_expose_hook(fl->win, draw_mandelbrot, fl);
+	else if (ft_strequ("julia", argv[1]))
+		mlx_expose_hook(fl->win, draw_julia, fl);
+	else if (ft_strequ("X", argv[1]))
+		mlx_expose_hook(fl->win, draw_X, fl);
+	fl->buffer.img = mlx_new_image(fdf->mlx, WIN_WIDTH, WIN_HEIGHT);
+	fl->buffer.data = mlx_get_data_addr(fl->buffer.img, &fl->buffer.bpp,
+			&fl->buffer.line_size, &fl->buffer.endian);
 
 }
 
@@ -26,13 +37,13 @@ static void		ft_check_args(int argc, char **argv)
 	}
 	else if (argc > 2)
 	{
-		ft_putendl_fd("Error, too much arguments.", 2);
+		ft_putendl_fd("Error, too many arguments.", 2);
 		exit(EXIT_FAILURE);
 	}
 	if (!(ft_strequ("mandelbrot", argv[1]) || ft_strequ("julia", argv[1]) ||
-				ft_strequ("3", argv[1])))
+				ft_strequ("X", argv[1])))
 	{
-		ft_putendl_fd("Error, plase specify a valid fractal: mandelbrot, julia or 3.", 2);
+		ft_putendl_fd("Error, plase specify a valid fractal: mandelbrot, julia or X.", 2);
 		exit(EXIT_FAILURE);
 	}
 }
@@ -42,7 +53,7 @@ int				main(int argc, char **argv)
 	t_fractol	fl;
 
 	ft_check_args(argc, argv);
-	ft_init(&fl);
+	ft_init(&fl, argv);
 	mlx_key_hook(fl.win, keyboard_event, &fl);
 	mlx_mouse_hook(fl.win, mouse_event, &fl);
 	mlx_loop();

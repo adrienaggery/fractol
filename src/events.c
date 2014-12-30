788125edbc1c4917b7a0938e1b7cb0491356f5d2
Adrien AGGERY <adrien@aggery.com>
2014-12-30 20:08:26 +0100

    new, added protos for mouse & keyboard

---
+++ src/events.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   events.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaggery <aaggery@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/12/30 20:00:31 by aaggery           #+#    #+#             */
+/*   Updated: 2014/12/30 20:06:36 by aaggery          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fractol.h"
+
+int		mouse_event(int button, int x, int y, t_fractol *fl)
+{
+	return (0);
+}
+
+int		keyboard_event(int keycode, t_fractol *fl)
+{
+	return (0);
+}

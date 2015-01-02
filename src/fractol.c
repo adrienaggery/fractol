3abbf93d91c3eb176dece6ab703cf1b1dcb44627
Adrien AGGERY <adrien@aggery.com>
2015-01-02 23:49:48 +0100

    modified temporary fractals names

--- src/fractol.c
+++ src/fractol.c
@@ -6,23 +6,32 @@
 /*   By: aaggery <aaggery@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/12/30 13:35:20 by aaggery           #+#    #+#             */
-/*   Updated: 2014/12/30 19:55:31 by aaggery          ###   ########.fr       */
+/*   Updated: 2015/01/02 23:44:13 by aaggery          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-static void		ft_init(t_fractol *fl)
+static void		ft_init(t_fractol *fl, char **argv)
 {
+	fl->name = argv[1];
+	fl->zoom = 1;
+	fl->offset.x = 0;
+	fl->offset.y = 0;
 	fl->mlx = mlx_init();
+	if (fl->mlx == NULL)
+	{
+		ft_putendl_fd("Error, failed to initiate mlx.", 2);
+		exit(EXIT_FAILURE);
+	}
 	fl->win = mlx_new_window(fl->mlx, WIN_WIDTH, WIN_HEIGHT, "Fract'ol");
 	if (ft_strequ("mandelbrot", argv[1]))
 		mlx_expose_hook(fl->win, draw_mandelbrot, fl);
 	else if (ft_strequ("julia", argv[1]))
-		mlx_expose_hook(fl->win, draw_julia, fl);
+		mlx_expose_hook(fl->win, draw_mandelbrot, fl);
 	else if (ft_strequ("X", argv[1]))
-		mlx_expose_hook(fl->win, draw_X, fl);
-	fl->buffer.img = mlx_new_image(fdf->mlx, WIN_WIDTH, WIN_HEIGHT);
+		mlx_expose_hook(fl->win, draw_mandelbrot, fl);
+	fl->buffer.img = mlx_new_image(fl->mlx, WIN_WIDTH, WIN_HEIGHT);
 	fl->buffer.data = mlx_get_data_addr(fl->buffer.img, &fl->buffer.bpp,
 			&fl->buffer.line_size, &fl->buffer.endian);
 
@@ -32,12 +41,12 @@ static void		ft_check_args(int argc, char **argv)
 {
 	if (argc < 2)
 	{
-		ft_putendl_fd("Error, please specify a fractal.", 2);
+		ft_putendl_fd("Error, please specify a fractal: mandelbrot, julia or X.", 2);
 		exit(EXIT_FAILURE);
 	}
 	else if (argc > 2)
 	{
-		ft_putendl_fd("Error, too many arguments.", 2);
+		ft_putendl_fd("Error, please specify a valid fractal: mandelbrot, julia or X.", 2);
 		exit(EXIT_FAILURE);
 	}
 	if (!(ft_strequ("mandelbrot", argv[1]) || ft_strequ("julia", argv[1]) ||
@@ -54,8 +63,8 @@ int				main(int argc, char **argv)
 
 	ft_check_args(argc, argv);
 	ft_init(&fl, argv);
-	mlx_key_hook(fl.win, keyboard_event, &fl);
-	mlx_mouse_hook(fl.win, mouse_event, &fl);
-	mlx_loop();
+	//mlx_key_hook(fl.win, keyboard_event, &fl);
+	//mlx_mouse_hook(fl.win, mouse_event, &fl);
+	mlx_loop(fl.mlx);
 	return (0);
 }

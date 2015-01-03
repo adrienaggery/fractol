d3df7384ce2d0e08f7b4ff1f31e347b5943e0ad0
Adrien AGGERY <adrien@aggery.com>
2015-01-03 22:44:09 +0100

    now handle ESC key to exit

--- src/events.c
+++ src/events.c
@@ -6,7 +6,7 @@
 /*   By: aaggery <aaggery@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/12/30 20:00:31 by aaggery           #+#    #+#             */
-/*   Updated: 2014/12/30 20:06:36 by aaggery          ###   ########.fr       */
+/*   Updated: 2015/01/03 19:19:12 by aaggery          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,17 @@
 
 int		mouse_event(int button, int x, int y, t_fractol *fl)
 {
+	(void)button;
+	(void)x;
+	(void)y;
+	(void)fl;
 	return (0);
 }
 
 int		keyboard_event(int keycode, t_fractol *fl)
 {
+	(void)fl;
+	if (keycode == KEYCODE_EXIT)
+		exit(EXIT_SUCCESS);
 	return (0);
 }

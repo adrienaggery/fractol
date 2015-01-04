89524afe41eeec6e6e84d18e3916751dfcd1cd39
Adrien AGGERY <adrien@aggery.com>
2015-01-05 00:30:26 +0100

    started work on personal algorithm

--- src/mandelbrot.c
+++ src/mandelbrot.c
@@ -6,28 +6,21 @@
 /*   By: aaggery <aaggery@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/12/31 17:42:47 by aaggery           #+#    #+#             */
-/*   Updated: 2015/01/04 18:45:19 by aaggery          ###   ########.fr       */
+/*   Updated: 2015/01/05 00:21:50 by aaggery          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-static int		ft_iterate(int max_it, t_imaginary_number p)
+static int		ft_iterate(int max_it, int x, int y)
 {
 	int						i;
-	t_imaginary_number		new;
-	t_imaginary_number		old;
 
 	i = 0;
-	new.real = 0;
-	new.imaginary = 0;
 	while (i < max_it)
 	{
 		i++;
-		old.real = new.real;
-		old.imaginary = new.imaginary;
-		new.real = old.real * old.real - old.imaginary * old.imaginary + p.real;
-		new.imaginary = 2 * old.real * old.imaginary + p.imaginary;
+
 		if ((new.real * new.real + new.imaginary * new.imaginary) > 4)
 			break ;
 	}
@@ -47,9 +40,7 @@ static void		ft_buff_image(t_fractol *fl)
 		x = 0;
 		while (x < WIN_WIDTH)
 		{
-			p.real = 1.5 * (x - WIN_WIDTH / 2) / (0.5 * fl->zoom * WIN_WIDTH) + fl->offset.x;
-			p.imaginary = (y - WIN_HEIGHT / 2) / (0.5 * fl->zoom * WIN_HEIGHT) + fl->offset.y;
-			i = ft_iterate(fl->max_it, p);
+			i = ft_iterate(fl->max_it, (x - fl->offset.x) * fl->zoom, (y - fl->offset.y) * fl->zoom);
 			//printf("i = %d (%d, %d): %#x\n", i, x, y ,ft_HSLtoHex(i, 1, 0.5));
 			ft_put_pixel_to_img(&fl->buffer, x, y, ft_HSLtoHex(i % 360, 1, 0.5 * (i < fl->max_it)));
 			x++;

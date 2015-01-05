aa468100a030c391735d12b16b8d918fb217b490
Adrien AGGERY <adrien@aggery.com>
2015-01-05 19:20:47 +0100

    modified algorithm, now plot the fractal

--- src/mandelbrot.c
+++ src/mandelbrot.c
@@ -6,23 +6,26 @@
 /*   By: aaggery <aaggery@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/12/31 17:42:47 by aaggery           #+#    #+#             */
-/*   Updated: 2015/01/05 00:21:50 by aaggery          ###   ########.fr       */
+/*   Updated: 2015/01/05 18:52:37 by aaggery          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-static int		ft_iterate(int max_it, int x, int y)
+static int		ft_iterate(int max_it, t_float2d c)
 {
 	int						i;
+	t_float2d				tmp;
+	float					x;
 
 	i = 0;
-	while (i < max_it)
+	tmp.x = 0;
+	tmp.y = 0;
+	while (i++ < max_it && (tmp.x * tmp.x + tmp.y * tmp.y) < 4)
 	{
-		i++;
-
-		if ((new.real * new.real + new.imaginary * new.imaginary) > 4)
-			break ;
+		x = tmp.x;
+		tmp.x = tmp.x * tmp.x - tmp.y * tmp.y + c.x;
+		tmp.y = (2 * x * tmp.y) + c.y;
 	}
 	return (i);
 }
@@ -32,7 +35,7 @@ static void		ft_buff_image(t_fractol *fl)
 	int						i;
 	int						x;
 	int						y;
-	t_imaginary_number		p;
+	t_float2d				c;
 
 	y = 0;
 	while (y < WIN_HEIGHT)
@@ -40,7 +43,9 @@ static void		ft_buff_image(t_fractol *fl)
 		x = 0;
 		while (x < WIN_WIDTH)
 		{
-			i = ft_iterate(fl->max_it, (x - fl->offset.x) * fl->zoom, (y - fl->offset.y) * fl->zoom);
+			c.x = (x - fl->offset.x) * (1 / fl->zoom);
+			c.y = (y - fl->offset.y) * (1 / fl->zoom);
+			i = ft_iterate(fl->max_it, c);
 			//printf("i = %d (%d, %d): %#x\n", i, x, y ,ft_HSLtoHex(i, 1, 0.5));
 			ft_put_pixel_to_img(&fl->buffer, x, y, ft_HSLtoHex(i % 360, 1, 0.5 * (i < fl->max_it)));
 			x++;
